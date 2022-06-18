6dee56d30c932f8dbec02f24739c39fe8416da37
HelloitsMyusernameiscodeing <107098436+HelloitsMyusernameiscodeing@users.noreply.github.com>
2022-06-18 13:51:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function cp(){
+document.getElementById("Image1").src="https://www.telegraph.co.uk/content/dam/football/2017/09/05/TELEMMGLPICT000137923891_trans_NvBQzQNjv4BqWZZ9520Qrn8RyVs0byqFfxGByaeD6X80N28ho092CK4.jpeg?imwidth=1400";
+}
